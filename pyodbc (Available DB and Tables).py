639f02d9d063af1963a62e3f9f8b4cb0d233820b
Raviraj Wagh <103710873+RAVIRAJWAGH@users.noreply.github.com>
2024-02-04 16:17:17 +0530

    Add files via upload

---
+++ pyodbc (Available DB and Tables).py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Connection
+
+# In[1]:
+
+
+import pyodbc
+
+driver="SQL Server"
+server = 'DESKTOP-HD9OHSA\MSSQLEXPRESS'
+
+connection = pyodbc.connect( f'DRIVER={{SQL Server}};SERVER={server};trusted_connection=yes')
+
+cursor = connection.cursor()
+
+
+# ## Available Database Names
+
+# In[3]:
+
+
+cursor.execute('SELECT name FROM sys.databases')
+cursor.fetchall()
+
+
+# ## Table Names
+# Tables available in that data base
+
+# In[4]:
+
+
+cursor.execute("USE master")
+cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE'")
+cursor.fetchall()
+
+
+# In[ ]:
+
+
+
+
