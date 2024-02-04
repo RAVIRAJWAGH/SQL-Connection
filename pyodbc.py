f228095bcb329b832798c34544e72a790f143c25
Raviraj Wagh <103710873+RAVIRAJWAGH@users.noreply.github.com>
2024-02-04 15:47:05 +0530

    Add files via upload

---
+++ pyodbc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pyodbc
+
+driver="SQL Server"
+server = 'DESKTOP-HD9OHSA\MSSQLEXPRESS'
+database = 'Database_name'
+
+connection = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};trusted_connection=yes')
+
+cursor = connection.cursor()
+
+cursor.execute('SELECT * FROM table_name')
+cursor.fetchall()
+
